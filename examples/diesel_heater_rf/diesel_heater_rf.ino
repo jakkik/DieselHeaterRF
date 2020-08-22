a4fcb98c2aa89076c585d14b9197348ed264c25d
Jarno Kyttälä <19662482+jakkik@users.noreply.github.com>
2020-08-22 14:59:21 +0300

    Update example sketch

--- examples/diesel_heater_rf/diesel_heater_rf.ino
+++ examples/diesel_heater_rf/diesel_heater_rf.ino
@@ -16,7 +16,7 @@
 
 #define HEATER_POLL_INTERVAL  4000
 
-uint32_t heaterAddr = 0x56d24eae; // Heater address is a 32 bit unsigned int. Use the findAddress() to get your heater's address.
+uint32_t heaterAddr; // Heater address is a 32 bit unsigned int. Use the findAddress() to get your heater's address.
 
 DieselHeaterRF heater;
 heater_state_t state;
@@ -25,16 +25,16 @@ void setup() {
 
   Serial.begin(115200);
 
-  heater.begin(heaterAddr);
+  heater.begin();
 
   Serial.println("Started pairing, press and hold the pairing button on the heater's LCD panel...");
 
-  uint32_t address = heater.findAddress(60000UL);
+  heaterAddr = heater.findAddress(60000UL);
 
-  if (address) {
-    heaterAddr = address;
+  if (heaterAddr) {
     Serial.print("Got address: ");
-    Serial.println(address, HEX);
+    Serial.println(heaterAddr, HEX);
+    heater.setAddress(heaterAddr);
     // Store the address somewhere, eg. NVS
   } else {
     Serial.println("Failed to find a heater");   
@@ -45,7 +45,7 @@ void setup() {
 
 void loop() {
 
-  heater.sendCommand(HEATER_CMD_WAKEUP, heaterAddr, 10);
+  heater.sendCommand(HEATER_CMD_WAKEUP);
 
   if (heater.getState(&state, 1000)) {
     Serial.printf("State: %d, Power: %d, Voltage: %f, Ambient: %d, Case: %d, Setpoint: %d, PumpFreq: %f, Auto: %d, RSSI: %d\n", state.state, state.power, state.voltage, state.ambientTemp, state.caseTemp, state.setpoint, state.pumpFreq, state.autoMode, state.rssi); 
